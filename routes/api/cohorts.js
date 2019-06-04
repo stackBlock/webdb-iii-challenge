1e3c78189b33bb288dd2eb3826b2ba074bcb96e8
anthony <stackchain@gmail.com>
2019-06-04 14:09:52 -0400

    commit - most of the mvp and slack api's working

--- routes/api/cohorts.js
+++ routes/api/cohorts.js
@@ -13,6 +13,51 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('cohorts').where({ id: id }).then(function (data) {
+        res.send(data);
+    })
+})
+
+
+router.post('/', (req, res) => {
+    const name = req.body;
+    db.insert(name)
+        .into('cohorts')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db('cohorts')
+        .where({ id: id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('cohorts')
+        .where({ id: id })
+        .del()
+        .then(count => {
+            res.status(200).json({ Message: `Deleted Cohort #${count}` });
+        });
+});
+
 
 
 
--- routes/api/students.js
+++ routes/api/students.js
@@ -14,6 +14,50 @@ router.get('/', (req, res) => {
 
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('students').where({ id: id }).then(function (data) {
+        res.send(data);
+    })
+})
+
+
+router.post('/', (req, res) => {
+    const name = req.body;
+    db.insert(name)
+        .into('students')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db('students')
+        .where({ id: id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('students')
+        .where({ id: id })
+        .del()
+        .then(count => {
+            res.status(200).json({ Message: `Deleted Student #${count}` });
+        });
+});
 
 
 module.exports = router;
